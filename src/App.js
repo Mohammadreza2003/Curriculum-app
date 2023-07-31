77727665b3a10c805430f4813f0ace27953563dc
Mohammadreza <75996851+Mohammadreza2003@users.noreply.github.com>
2023-07-31 18:51:07 +0330

    fixed some bugs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import Curriculum from "./Curriculum";
-import PresenceAbsence from "./PresenceAbsence";
 import TeachersList from "./TeachersList";
 import ClassesList from "./ClassesList";
-import "./App.css"
+import "./App.css";
 const App = () => {
   return (
     <>
-      <Curriculum />
-      <PresenceAbsence />
       <TeachersList />
+      <Curriculum />
       <ClassesList />
     </>
   );
